017216eabb5b4c0fe92cae62e934adcb2869fa9f
ilijastarcevic96 <starcevicfon@gmail.com>
2017-03-22 14:42:06 +0100

    Uvedena klasa Biblioteka sa osnovnim implementacijama

---
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -0,0 +1,43 @@
+package biblioteka;
+
+import java.util.LinkedList;
+
+import biblioteka.intefejs.BibliotekaInterfejs;
+
+public class Biblioteka implements BibliotekaInterfejs {
+	
+	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
+
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		knjige.add(knjiga);
+
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		knjige.remove(knjiga);
+
+	}
+
+	@Override
+	public LinkedList<Knjiga> vratiSveKnjige() {
+		
+		return knjige;
+	}
+
+	@Override
+	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
+		
+		LinkedList<Knjiga> rezultati = new LinkedList<Knjiga>();
+		
+		for (int i = 0; i < knjige.size(); i++) {
+			if(knjige.get(i).getNaslov().contains(naslov)){
+				rezultati.add(knjige.get(i));
+			}
+		}
+		
+		return rezultati;
+	}
+
+}
